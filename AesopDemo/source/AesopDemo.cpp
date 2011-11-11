ddc0240d7b7b6c145fc3e1f7d994139052e1b4ec
dan.buckmaster@gmail.com <dan.buckmaster@gmail.com@cc488be4-ec2e-0622-d0b9-0d698bc312c8>
2011-11-11 02:39:04 +0000

    Updated the demo code to make use of Actions and use clearer variable names. There is now a small scenario defined.

--- AesopDemo/source/AesopDemo.cpp
+++ AesopDemo/source/AesopDemo.cpp
@@ -8,28 +8,66 @@
 
 int main(int argc, char **argv)
 {
-   // Create several predicate names and values.
-   ae::PName pred1 = "Pred1";
-   ae::PName pred2 = "Pred2";
-   ae::PName pred3 = "Pred3";
-   ae::PVal val1 = 3;
-   ae::PVal val2 = 4;
-   ae::PVal val3 = 'u';
+   // Create several Predicate names.
+   ae::PName at = "at";
+   ae::PName hungry = "hungry";
+   ae::PName money = "money";
 
-   // Create a WorldState, set pred1 to 3 and initialise pred2 as unset.
+   // Boolean predicate values.
+   ae::PVal t = 1;
+   ae::PVal f = 0;
+
+   // Three location names.
+   ae::PVal loc1 = 'a';
+   ae::PVal loc2 = 'b';
+   ae::PVal loc3 = 'c';
+
+   // Create a WorldState with a location specified.
    ae::WorldState ws1;
-   ws1.setPredicate(pred1, val1);
-   ws1.unsetPredicate(pred2);
+   ws1.setPredicate(at, loc1);
 
-   // Create another WorldState copied from the first and make some changes.
-   ae::WorldState ws2 = ws1;
-   ws2.setPredicate(pred1, val2);
-   ws2.setPredicate(pred3, val3);
+   // Create another WorldState with different data.
+   ae::WorldState ws2;
+   ws2.setPredicate(at, loc2);
+   ws2.setPredicate(money, t);
 
    // Print out the number of differences between ws1 and ws2.
    printf("Comparison of ws1 and ws2: %d\n", ae::WorldState::comp(ws1, ws2));
    // Should have printed 1, since ws1 and ws2 differ only once, in the value
    // of pred1. (Predicates not set in both states are not counted.)
+
+   // Create some Actions to move between the three locations.
+   //   Required: we are in some location
+   //   Outcome:  we are in some location adjacent
+   ae::Action aMove1;
+   aMove1.addRequired(ae::Statement(at, loc1));
+   aMove1.addSet(ae::Statement(at, loc2));
+   ae::Action aMove2;
+   aMove2.addRequired(ae::Statement(at, loc2));
+   aMove2.addSet(ae::Statement(at, loc3));
+   ae::Action aMove3;
+   aMove3.addRequired(ae::Statement(at, loc3));
+   aMove3.addSet(ae::Statement(at, loc2));
+   ae::Action aMove4;
+   aMove4.addRequired(ae::Statement(at, loc2));
+   aMove4.addSet(ae::Statement(at, loc1));
+
+   // Action to take money from loc3.
+   //   Required: we are at loc3 and have no money
+   //   Outcome:  we have money
+   ae::Action aTake;
+   aTake.addRequired(ae::Statement(at, loc3));
+   aTake.addRequired(ae::Statement(money, f));
+   aTake.addSet(ae::Statement(money, t));
+
+   // Action to order food from loc2.
+   //   Required: we are at loc2 and have money
+   //   Outcome:  we have no money and are not hungry
+   ae::Action aOrder;
+   aOrder.addRequired(ae::Statement(at, loc2));
+   aOrder.addRequired(ae::Statement(money, t));
+   aOrder.addSet(ae::Statement(money, f));
+   aOrder.addSet(ae::Statement(hungry, f));
 
    return 0;
 }
