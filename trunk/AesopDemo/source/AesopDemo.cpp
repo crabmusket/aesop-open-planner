435734237d33b66ce2b8957f680fbb41b25bf0fe
dan.buckmaster@gmail.com <dan.buckmaster@gmail.com@cc488be4-ec2e-0622-d0b9-0d698bc312c8>
2011-11-16 12:54:42 +0000

    Cleaned up demo code and documentation.

--- trunk/AesopDemo/source/AesopDemo.cpp
+++ trunk/AesopDemo/source/AesopDemo.cpp
@@ -32,37 +32,6 @@ int main(int argc, char **argv)
    ae::WorldState goal;
    goal.setPredicate(hungry, pfalse);
 
-   // Movement action.
-   //   Required: we are at location given by param 0
-   //   Outcome: we are at location given by param 1
-   MoveAction aMove("Move");
-   aMove.addRequiredParam(at, 0);
-   aMove.addSetParam(at, 1);
-
-   // Create some Actions to move between the three locations.
-   //   Required: we are in some location
-   //   Outcome:  we are in some location adjacent
-   ae::DefaultAction aMove1("Move A->B");
-   aMove1.addRequired(at, loc1);
-   aMove1.addSet(at, loc2);
-   ae::DefaultAction aMove2("Move B->C");
-   aMove2.addRequired(at, loc2);
-   aMove2.addSet(at, loc3);
-   ae::DefaultAction aMove3("Move C->B");
-   aMove3.addRequired(at, loc3);
-   aMove3.addSet(at, loc2);
-   ae::DefaultAction aMove4("Move B->A");
-   aMove4.addRequired(at, loc2);
-   aMove4.addSet(at, loc1);
-
-   // Action to take money from loc3.
-   //   Required: we are at loc3 and have no money
-   //   Outcome:  we have money
-   ae::DefaultAction aTake("Take money");
-   aTake.addRequired(at, loc3);
-   aTake.addRequired(money, pfalse);
-   aTake.addSet(money, ptrue);
-
    // Action to buy food from loc2.
    //   Required: we are at loc2 and have money
    //   Outcome:  we have no money and are not hungry
@@ -72,12 +41,23 @@ int main(int argc, char **argv)
    aOrder.addSet(money, pfalse);
    aOrder.addSet(hungry, pfalse);
 
+   // Action to take money from loc3.
+   //   Required: we are at loc3 and have no money
+   //   Outcome:  we have money
+   ae::DefaultAction aTake("Take money");
+   aTake.addRequired(at, loc3);
+   aTake.addRequired(money, pfalse);
+   aTake.addSet(money, ptrue);
+
+   // Movement action.
+   //   Required: we are at location given by param 0
+   //   Outcome: we are at location given by param 1
+   MoveAction aMove("Move");
+   aMove.addRequiredParam(at, 0);
+   aMove.addSetParam(at, 1);
+
    // Bundle these actions into an ActionSet.
    ae::ActionSet actions;
-   //actions.push_back(&aMove1);
-   //actions.push_back(&aMove2);
-   //actions.push_back(&aMove3);
-   //actions.push_back(&aMove4);
    actions.push_back(&aMove);
    actions.push_back(&aTake);
    actions.push_back(&aOrder);
@@ -139,28 +119,36 @@ AesopDemoLogger::~AesopDemoLogger()
 
 /// @class MoveAction
 ///
-/// A simple implementation of ae::Action.
+/// A simple implementation of ae::Action. Its purpose is to provide a generic
+/// 'move' Action for AesopDemo. It has two parameters, one for the starting
+/// location and one for the destination. It fills in the starting parameter
+/// based on locations adjacent to the end point. It is simplistically
+/// hard-coded to the three locations 'A', 'B' and 'C' used in the demo app.
 
-/// Subclasses of ae::Action are wise to call its constructor with their
-/// values of name and cost. They are encouraged to set their own value
-/// of mNumParams, as particular to this implementation of an action.
+/// Subclasses of ae::Action are wise to call its constructor with their values
+/// of name and cost. They are encouraged to set their own value of mNumParams,
+/// as particular to this implementation of an action.
 MoveAction::MoveAction(std::string name, float cost)
 : ae::Action(name, cost)
 {
    mNumParams = 2;
 }
 
+/// Check to see what finishing parameter has been specified by the planner.
+/// Fill in permutations of starting locations as appropriate.
 void MoveAction::getParams(const ae::paramlist &plist, ae::paramset &pset) const
 {
    pset.clear();
    switch(plist[1])
    {
    case 'A':
+      // If we must finish at A, we only have one option: leave from B.
       pset.push_back(ae::paramlist(mNumParams));
       pset.back()[0] = 'B';
       pset.back()[1] = 'A';
       break;
    case 'B':
+      // If we finish at B, we may have arrived from A or C.
       pset.push_back(ae::paramlist(mNumParams));
       pset.back()[0] = 'A';
       pset.back()[1] = 'B';
@@ -169,6 +157,7 @@ void MoveAction::getParams(const ae::paramlist &plist, ae::paramset &pset) const
       pset.back()[1] = 'B';
       break;
    case 'C':
+      // Finishing at C, we must have started at B.
       pset.push_back(ae::paramlist(mNumParams));
       pset.back()[0] = 'B';
       pset.back()[1] = 'C';
