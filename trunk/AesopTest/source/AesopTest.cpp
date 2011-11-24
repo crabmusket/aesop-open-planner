3c8e61620be03c1b5fbbfbf55ccc3ad4b6d58e99
dan.buckmaster@gmail.com <dan.buckmaster@gmail.com@cc488be4-ec2e-0622-d0b9-0d698bc312c8>
2011-11-24 15:11:00 +0000

    Added a unit testing module using googletest 1.6.0.

---
+++ trunk/AesopTest/source/AesopTest.cpp
@@ -0,0 +1,11 @@
+/// @file AesopTest.cpp
+/// @brief Main file of AesopTest module.
+
+#include "gtest/gtest.h"
+#include "AesopTest.h"
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
