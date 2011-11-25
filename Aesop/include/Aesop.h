73662b99a22fdf8500037463a587b74e78718214
dan.buckmaster@gmail.com <dan.buckmaster@gmail.com@cc488be4-ec2e-0622-d0b9-0d698bc312c8>
2011-11-25 09:38:56 +0000

    Moved mainpage stuff into its own file in AesopDoc. Fixed up the Doxyfile a bit.

--- Aesop/include/Aesop.h
+++ Aesop/include/Aesop.h
@@ -23,28 +23,6 @@
 /// @file Aesop.h
 /// @brief Main file for Aesop open planning library.
 
-/// @mainpage
-/// Aesop is a library of classes and functions that implement a STRIPS-like
-/// dynamic planner in C++. The goal of the planner is to take an initial world
-/// state, a set of actions that operate on the world state, and put together
-/// a sequence of actions such that the world is transformed into some desired
-/// final state.
-///
-/// This library is open-source under the MIT license. Get the source code at:
-/// <a href="http://code.google.com/p/aesop-open-planner">
-/// http://code.google.com/p/aesop-open-planner</a>
-///
-/// For more information:
-/// <ul>
-///   <li>
-///        <a href="http://en.wikipedia.org/wiki/STRIPS">STRIPS - Wikipedia</a>
-///   </li>
-///   <li>
-///        <a href="http://web.media.mit.edu/~jorkin/goap.html">Goal-Oriented
-///        Action Planning</a>
-///   </li>
-/// </ul>
-
 #ifndef _AE_AESOP_H_
 #define _AE_AESOP_H_
 
---
+++ AesopDoc/mainpage.h
@@ -0,0 +1,23 @@
+///
+/// @mainpage
+/// Aesop is a library of classes and functions that implement a STRIPS-like
+/// dynamic planner in C++. The goal of the planner is to take an initial world
+/// state, a set of actions that operate on the world state, and put together
+/// a sequence of actions such that the world is transformed into some desired
+/// final state.
+///
+/// This library is open-source under the MIT license. Get the source code at:
+/// <a href="http://code.google.com/p/aesop-open-planner">
+/// http://code.google.com/p/aesop-open-planner</a>
+///
+/// For more information:
+/// <ul>
+///   <li>
+///        <a href="http://en.wikipedia.org/wiki/STRIPS">STRIPS - Wikipedia</a>
+///   </li>
+///   <li>
+///        <a href="http://web.media.mit.edu/~jorkin/goap.html">Goal-Oriented
+///        Action Planning</a>
+///   </li>
+/// </ul>
+///
