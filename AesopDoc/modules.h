d042dea643afb89e4568a62c414845cdd9ca82cb
dan.buckmaster@gmail.com <dan.buckmaster@gmail.com@cc488be4-ec2e-0622-d0b9-0d698bc312c8>
2011-11-25 11:45:41 +0000

    Added documentation to modules.

--- AesopDoc/modules.h
+++ AesopDoc/modules.h
@@ -1,11 +1,25 @@
 ///
 /// @defgroup Aesop Aesop
 ///
+/// The Aesop module contains the planner library code. When integrating Aesop
+/// into your own project, this is the code you'll need to work with.
+///
 
 ///
 /// @defgroup AesopDemo AesopDemo
 ///
+/// AesopDemo is a small example application that provides a practical
+/// implementation of Aesop's features. It is primarily used as a testbed for
+/// deleopment, until a full suite of unit tests is available, as well as a
+/// proof of functionality.
+///
 
 ///
 /// @defgroup AesopTest AesopTest
+///
+/// This module contains a suite of unit tests written with googletest. They
+/// are used when developing, to make sure that modifications to the Aesop
+/// classes do not break its intended functionality. If you don't plan on
+/// modifying any of the source code in the Aesop module, then you shouldn't
+/// need to worry about any of this code.
 ///
