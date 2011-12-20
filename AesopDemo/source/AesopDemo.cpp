fdce352fc40db8eec2f8ca4b9d1981f08e95e5b8
dan.buckmaster@gmail.com <dan.buckmaster@gmail.com@cc488be4-ec2e-0622-d0b9-0d698bc312c8>
2011-12-20 22:33:11 +0000

    Continued drafting ideas for how the demo code should look.

--- AesopDemo/source/AesopDemo.cpp
+++ AesopDemo/source/AesopDemo.cpp
@@ -31,17 +31,64 @@ int main(int argc, char **argv)
 
    // Require that objects can have types.
    domain.addRequirement(ae::typing);
+   // Require the use of the equality predicate.
+   domain.addRequirement(ae::equality);
 
    // Add the types relevant to our problem domain.
    domain.addType("room");
-   domain.addType("money");
-   domain.addType("vending-machine");
+   domain.addType("physob");
+   // The next four types are subtypes of physob, whereas room and physob are
+   // subtypes of object by default.
+   domain.addType("money"/*, "physob"*/);
+   domain.addType("vending-machine"/*, "physob"*/);
+   domain.addType("person"/*, "physob"*/);
+
+   // Define predicates.
+   //ae::Predicate *pr;
+
+   // Define adjacency of rooms.
+   //pr = domain.addPredicate("adjacent");
+   //pr->addParameter("f", "room");
+   //pr->addParameter("t", "room");
+
+   // Define an object being in a room.
+   //pr = domain.addPredicate("in");
+   //pr->addParameter("x", "physob");
+   //pr->addParameter("y", "room");
+
+   // Is somebody hungry?
+   //pr = domain.addPredicate("hungry");
+   //pr->addParameter("w", "person");
+
+   // Do we have food?
+   //pr = domain->addPredicate("havefood");
+   //pr->addParameter("w", "person");
 
    // Create some Actions to solve problems with.
    ae::Action *ac;
 
    // Move Action.
    ac = domain.addAction("move", 1.0f);
+   //ac->addParameter("who", "person");
+   //ac->addParameter("from", "room");
+   //ac->addParameter("to",   "room");
+   //ac->addCondition(true, "in", "who", "from");
+   //ac->addCondition(false, "in", "who", "to");
+   //ac->addCondition(true, "adjacent", "from", "to");
+   //ac->addEffect(false, "in", "who", "from");
+   //ac->addEffect(true, "in", "who", "to");
+
+   // Take money.
+
+   // Buy food.
+
+   // Eat food.
+   //ac = domain.addAction("eat", 1.0f);
+   //ac->addParameter("who", "person");
+   //ac->addCondition(true, "havefood", "who");
+   //ac->addCondition(true, "hungry", "who");
+   //ac->addEffect(false, "havefood", "who");
+   //ac->addEffect(false, "hungry", "who");
 
    // Create a Problem within our domain.
    ae::Problem problem(domain);
@@ -52,10 +99,12 @@ int main(int argc, char **argv)
    problem.addObject("roomC", "room");
 
    // Add our money.
-   problem.addObject("themoney", "money");
+   problem.addObject("money", "money");
 
    // And our vending machine.
    problem.addObject("vendor", "vending-machine");
+
+   // 
 
    return 0;
 }
