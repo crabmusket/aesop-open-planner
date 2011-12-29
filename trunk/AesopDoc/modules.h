a09f1629035bd9cd7d415978f042aaef9873431b
dan.buckmaster@gmail.com <dan.buckmaster@gmail.com@cc488be4-ec2e-0622-d0b9-0d698bc312c8>
2011-12-29 10:27:38 +0000

    Added detail to external documentation on Aesop module, and copied mainpage content from Google Code.

--- trunk/AesopDoc/modules.h
+++ trunk/AesopDoc/modules.h
@@ -2,8 +2,42 @@
 /// @defgroup Aesop Aesop
 /// @brief Aesop planning library.
 ///
-/// The Aesop module contains the planner library code. When integrating Aesop
-/// into your own project, this is the code you'll need to work with.
+/// <p>This module contains the Aesop planning library, all you need to add
+/// automated planning to your own project.</p>
+///
+/// <p>To create a domain, problem and plan using Aesop, you'll need to do
+/// the following. I recommend that these steps be performed in the order
+/// they're given, to prevent odd behaviour. If you know what you're doing,
+/// there's nothing to stop you adding requirements to a Domain mid-plan...
+/// but if you're doing that it's likely not what you intended.</p>
+/// <ol>
+///    <li>Create an ae::Domain, optionally passing it a name to uniquely
+///        identify it. A Domain is the representation of a problem space;
+///        it describes a particular class of problem, as we'll see below:
+///        <ol>
+///            <li>Call ae::Domain::addRequirement to define the requirements
+///                of the Domain. The requirements specify the features
+///                your Domain can support, and will affect its performance.</li>
+///            <li>Use ae::Domain::addType to define the object types that
+///                exist in the problem Domain. The types of a Domain are
+///                a hierarchy, where all types are children of "object".</li>
+///            <li>ae::Domain::addPredicate can then be called to define
+///                the Domain's predicates. Predicates are the properties
+///                that describe the world state. For example, to define the
+///                <i>in</i> predicate for an "object" and a "container"
+///                allows the Domain to express the idea that some object
+///                is inside some other object, which must be a type of
+///                container.</li>
+///            <li>Finally, ae::Domain::addAction is used to add Actions
+///                that are used to solve problems within the Domain. For
+///                more information on Actions, @see ae::Action.</li>
+///        </ol>
+///    </li>
+///    <li></li>
+///    <li></li>
+///    <li></li>
+///    <li></li>
+/// </ol>
 ///
 
 ///
