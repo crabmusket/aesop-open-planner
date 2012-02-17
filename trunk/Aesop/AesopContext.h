8457523af86edf13422c219512eebbf54f60529b
dan.buckmaster@gmail.com <dan.buckmaster@gmail.com@cc488be4-ec2e-0622-d0b9-0d698bc312c8>
2012-02-17 20:09:24 +0000

    Added profiling to Context and a NullContext class so that Context can be pure abstract.

--- trunk/Aesop/AesopContext.h
+++ trunk/Aesop/AesopContext.h
@@ -5,6 +5,7 @@
 #define _AE_CONTEXT_H_
 
 #include <stdio.h>
+#include <time.h>
 #include "AesopProblem.h"
 
 namespace Aesop {
@@ -12,12 +13,29 @@ namespace Aesop {
    /// @ingroup Aesop
    class Context {
    public:
+      virtual void success() = 0;
+      virtual void failure() = 0;
+      virtual void toClosed(unsigned int ID) = 0;
+      virtual void newState(const Problem::openstate &s) = 0;
+      virtual void beginPlanning() = 0;
+      virtual void beginIteration() = 0;
+      virtual void endIteration() = 0;
+      virtual void endPlanning() = 0;
+   protected:
+   private:
+   };
+
+   /// @brief Context that does nothing.
+   class NullContext : public Context {
+   public:
       virtual void success() {}
       virtual void failure() {}
       virtual void toClosed(unsigned int ID) {}
       virtual void newState(const Problem::openstate &s) {}
-   protected:
-   private:
+      virtual void beginPlanning() {}
+      virtual void beginIteration() {}
+      virtual void endIteration() {}
+      virtual void endPlanning() {}
    };
 
    /// @brief Example implementation of Context that writes output to a file.
@@ -30,6 +48,10 @@ namespace Aesop {
       virtual void failure();
       virtual void toClosed(unsigned int ID);
       virtual void newState(const Problem::openstate &s);
+      virtual void beginPlanning();
+      virtual void beginIteration();
+      virtual void endIteration();
+      virtual void endPlanning();
 
       /// @}
 
@@ -40,6 +62,12 @@ namespace Aesop {
    private:
       /// @brief File handle to write to.
       FILE &mFile;
+
+      /// @brief Time at start of planning.
+      clock_t mPlanStart;
+
+      /// @brief Number of iterations performed.
+      unsigned int mIters;
    };
 };
 
