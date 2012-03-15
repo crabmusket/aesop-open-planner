71b6ca7df5a5957357352cd73663fbad23563c18
dan.buckmaster@gmail.com <dan.buckmaster@gmail.com@cc488be4-ec2e-0622-d0b9-0d698bc312c8>
2012-03-15 07:57:43 +0000

    Updated demo code to backend changes. Solution compiles, but doesn't work thanks to Planner.

--- branches/goap2/AesopDemo/source/AesopDemo.cpp
+++ branches/goap2/AesopDemo/source/AesopDemo.cpp
@@ -16,12 +16,12 @@ void printPlan(Plan plan)
    for(it = plan.begin(); it != plan.end(); it++)
    {
       printf("   %s", it->ac->getName().c_str());
-      if(it->params.size())
-      {
-         paramlist::const_iterator pl;
-         for(pl = it->params.begin(); pl != it->params.end(); pl++)
-            printf(" %c", *pl);
-      }
+      //if(it->params.size())
+      //{
+      //   paramlist::const_iterator pl;
+      //   for(pl = it->params.begin(); pl != it->params.end(); pl++)
+      //      printf(" %c", *pl);
+      //}
       putchar('\n');
    }
 }
@@ -46,47 +46,48 @@ int main(int argc, char **argv)
 
    // Create a WorldState to represent our initial state.
    WorldState start;
-   start.setPredicate(at, loc1);
-   start.setPredicate(money, pfalse);
+   start.set(Fact(at), loc1);
+   start.set(Fact(hungry), ptrue);
+   start.set(Fact(money), pfalse);
 
    // Create another WorldState which will be our goal.
    WorldState goal;
-   goal.setPredicate(hungry, pfalse);
+   goal.set(Fact(hungry), pfalse);
 
    // Action to buy food from loc2.
    //   Required: we are at loc2 and have money
    //   Outcome:  we have no money and are not hungry
    Action aOrder("Buy food");
-   aOrder.addRequired(at, loc2);
-   aOrder.addRequired(money, ptrue);
-   aOrder.addSet(money, pfalse);
-   aOrder.addSet(hungry, pfalse);
+   aOrder.condition(Fact(at), Equals, loc1);
+   aOrder.condition(Fact(money), Equals, ptrue);
+   aOrder.effect(Fact(money), Set, pfalse);
+   aOrder.effect(Fact(hungry), Set, pfalse);
 
    // Action to take money from loc3.
    //   Required: we are at loc3 and have no money
    //   Outcome:  we have money
    Action aTake("Take money");
-   aTake.addRequired(at, loc3);
-   aTake.addRequired(money, pfalse);
-   aTake.addSet(money, ptrue);
+   aTake.condition(Fact(at), Equals, loc3);
+   aTake.condition(Fact(money), IsUnset);
+   aTake.effect(Fact(money), Set);
 
    // Movement action.
    //   Required: we are at location given by param 0
    //   Outcome: we are at location given by param 1
-   MoveAction aMove("Move");
-   aMove.addRequiredParam(at, 0);
-   aMove.addSetParam(at, 1);
+   //MoveAction aMove("Move");
+   //aMove.condition(Fact(at), ;
+   //aMove.addSetParam(at, 1);
 
    // Flying movement action.
    //   Required: we are at location given by param 0
    //   Outcome: we are at location given by param 1
-   FlyAction aFly("Fly", 1.5f);
-   aFly.addRequiredParam(at, 0);
-   aFly.addSetParam(at, 1);
+   //FlyAction aFly("Fly", 1.5f);
+   //aFly.addRequiredParam(at, 0);
+   //aFly.addSetParam(at, 1);
 
    // Bundle these actions into an ActionSet.
    ActionSet actions;
-   actions.add(&aMove);
+   //actions.add(&aMove);
    actions.add(&aTake);
    actions.add(&aOrder);
 
@@ -108,7 +109,7 @@ int main(int argc, char **argv)
    putchar('\n');
 
    // Make a plan for a flying character.
-   actions.add(&aFly);
+   //actions.add(&aFly);
    printf("Planning with flying behaviour!\n");
    if(planner.plan(&context))
    {
@@ -122,7 +123,7 @@ int main(int argc, char **argv)
    putchar('\n');
 
    // Now reduce the cost of flying (i.e., make it more preferable).
-   actions.add(&aFly, 0.5f);
+   //actions.add(&aFly, 0.5f);
    printf("Planning when we prefer to fly.\n");
    if(planner.plan(&context))
    {
