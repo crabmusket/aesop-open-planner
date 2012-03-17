9ea4f23c11edf0c5fa1d391bd1d762e216941cb3
dan.buckmaster@gmail.com <dan.buckmaster@gmail.com@cc488be4-ec2e-0622-d0b9-0d698bc312c8>
2012-03-17 06:47:03 +0000

    Added some unit tests to figure out where Fact was going wrong.

---
+++ branches/goap2/AesopTest/include/AesopFactTest.h
@@ -0,0 +1,55 @@
+/// @file AesopActionTest.h
+/// @brief gtest cases for Action class.
+
+#include "gtest/gtest.h"
+#include "Aesop.h"
+
+using namespace Aesop;
+
+/// @brief Test fixture for the Fact struct.
+/// @ingroup AesopTest
+class FactTest : public ::testing::Test {
+protected:
+   FactTest()
+   {
+   }
+};
+
+/*
+TEST_F(FactTest, )
+{
+   
+}
+*/
+
+TEST_F(FactTest, Constructor)
+{
+   PName pred = 3;
+   Fact f1(pred);
+   EXPECT_EQ(f1.name, pred);
+   EXPECT_EQ(f1.params.size(), 0);
+
+   Fact f2(pred, 2);
+   EXPECT_EQ(f2.name, pred);
+   EXPECT_EQ(f2.params.size(), 2);
+}
+
+TEST_F(FactTest, Assignment)
+{
+   PName pred = 3;
+   Fact *f1 = new Fact(pred, 2);
+   ASSERT_TRUE(f1 != NULL);
+   delete f1;
+}
+
+TEST_F(FactTest, Parameters)
+{
+   // Create a predicate name and some objects.
+   PName pred = 4;
+   Object o1 = 10, o2 = 3;
+   // Create a Fact with 2 parameters containing our objects.
+   Fact f1 = Fact(pred, 2) % o1 % o2;
+   ASSERT_EQ(f1.params.size(), 2);
+   EXPECT_EQ(f1.params[0], o1);
+   EXPECT_EQ(f1.params[1], o2);
+}
