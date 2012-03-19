bff49c918426896c2254d6d300c0c83641bd4c2f
dan.buckmaster@gmail.com <dan.buckmaster@gmail.com@cc488be4-ec2e-0622-d0b9-0d698bc312c8>
2012-03-19 23:07:05 +0000

    Updated unit tests, sort of.

--- branches/goap2/AesopTest/include/AesopActionTest.h
+++ branches/goap2/AesopTest/include/AesopActionTest.h
@@ -4,80 +4,45 @@
 #include "gtest/gtest.h"
 #include "Aesop.h"
 
-using namespace ae;
+using namespace Aesop;
 
 /// @brief Test fixture for the Action class.
 /// @ingroup AesopTest
 class ActionTest : public ::testing::Test {
 protected:
-   Action ac;
-   PName pn1;
-   PName pn2;
-   PVal pfalse;
-   PVal ptrue;
-
    ActionTest()
-      :ac("action", 1.0f),
-      pn1("pred1"),
-      pn2("pred2"),
-      pfalse(0),
-      ptrue(1)
    {
    }
 };
 
 TEST_F(ActionTest, Constructor)
 {
-   // Name and cost should be as specified.
-   EXPECT_EQ(ac.getName(), std::string("action"));
+   Action ac;
+   EXPECT_EQ(ac.getName(), "");
    EXPECT_EQ(ac.getCost(), 1.0f);
-   // 0 parameters by default.
    EXPECT_EQ(ac.getNumParams(), 0);
-   // All lists should be empty.
-   EXPECT_TRUE(ac.getRequired().empty());
-   EXPECT_TRUE(ac.getSet().empty());
-   EXPECT_TRUE(ac.getCleared().empty());
-   EXPECT_TRUE(ac.getRequiredParams().empty());
-   EXPECT_TRUE(ac.getSetParams().empty());
+   EXPECT_EQ(ac.begin(), ac.end());
 }
 
-TEST_F(ActionTest, RequiredPredicates)
+TEST_F(ActionTest, Named)
 {
-   // Add a single predicate, check for correctness.
-   ac.addRequired(pn1, pfalse);
-   EXPECT_FALSE(ac.getRequired().empty());
-   EXPECT_EQ(ac.getRequired().at(pn1), pfalse);
-   // Overwrite predicate, check for correctness.
-   ac.addRequired(pn1, ptrue);
-   EXPECT_EQ(ac.getRequired().size(), 1);
-   EXPECT_EQ(ac.getRequired().at(pn1), ptrue);
-   // Add a new predicate and check for correctness.
-   ac.addRequired(pn2, ptrue);
-   EXPECT_EQ(ac.getRequired().size(), 2);
-   EXPECT_EQ(ac.getRequired().at(pn2), ptrue);
+   Action ac("test", 2.0f);
+   EXPECT_EQ(ac.getName(), "test");
+   EXPECT_EQ(ac.getCost(), 2.0f);
+   EXPECT_EQ(ac.getNumParams(), 0);
+   EXPECT_EQ(ac.begin(), ac.end());
 }
 
-TEST_F(ActionTest, EffectPredicates)
+TEST_F(ActionTest, Conditions)
 {
-   // Add a single predicate, check for correctness.
-   ac.addSet(pn1, pfalse);
-   EXPECT_FALSE(ac.getSet().empty());
-   EXPECT_EQ(ac.getSet().at(pn1), pfalse);
-   // Overwrite predicate, check for correctness.
-   ac.addSet(pn1, ptrue);
-   EXPECT_EQ(ac.getSet().size(), 1);
-   EXPECT_EQ(ac.getSet().at(pn1), ptrue);
-   // Add a new predicate and check for correctness.
-   ac.addSet(pn2, ptrue);
-   EXPECT_EQ(ac.getSet().size(), 2);
-   EXPECT_EQ(ac.getSet().at(pn2), ptrue);
-   // Add a PName to clear.
-   ac.addClear(pn1);
-   EXPECT_EQ(ac.getCleared().size(), 1);
-   // Add the same entry; it should be rejected.
-   ac.addClear(pn1);
-   EXPECT_EQ(ac.getCleared().size(), 1);
-   // Add a new entry; the cleared list should grow.
-   ac.addClear(pn2);
-   EXPECT_EQ(ac.getCleared().size(), 2);
+   Action ac;
+   Fact f1(0);
+   ac.condition(f1, IsSet, 1);
+   ASSERT_NE(ac.begin(), ac.end());
+   const Fact &f = ac.begin()->first;
+   const Operation &op = ac.begin()->second;
+   ASSERT_EQ(f, f1);
+   ASSERT_EQ(op.ctype, IsSet);
+   ASSERT_EQ(op.cval, 1);
+   ASSERT_EQ(op.etype, NoEffect);
 }
